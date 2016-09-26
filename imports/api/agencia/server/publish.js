673ccab1363dbff7155f59711410c4303b834008
jalevtz <j.vlt.mtz@gmail.com>
2016-09-26 17:11:16 -0500

    se modifica  selector

--- imports/api/agencia/server/publish.js
+++ imports/api/agencia/server/publish.js
@@ -1,11 +1,11 @@
 import {Meteor} from "meteor/meteor";
-import {Agencia} from "./collection";
+import {Agencia} from "../collection";
 
 if (Meteor.isServer) {
     Meteor.publish('angencia', function () {
 
         const selector = {
-            _id: this.userId
+            propietario: this.userId
         };
         return Agencia.findOne(selector);
     });
