9259b9d77dcd3d18695e50f307ce026be01fd784
hEctor <hector.kaizen@gmail.com>
2016-11-10 12:26:50 -0600

    Correcciones 1
    - Formato Web Storm.
    - Se corrigió el Regex.
    - Se activó  la validación.
    - Se eliminó un console log y una función que no se usa.

--- imports/ui/components/vacantes/agregar/agregarTiendas/agregarTiendas.js
+++ imports/ui/components/vacantes/agregar/agregarTiendas/agregarTiendas.js
@@ -9,9 +9,9 @@ import {insertTienda} from "../../../../../api/tiendas/methods";
 import {Cadenas} from "../../../../../api/cadenas/collection";
 import {Session} from "meteor/session";
 import {ValidationError} from "meteor/mdg:validation-error";
-import {Estados} from '../../../../../api/estados/collection';
-import {Puestos} from '../../../../../api/puestos/collection';
-import {Escuelas} from '../../../../../api/escuelas/collection';
+import {Estados} from "../../../../../api/estados/collection";
+import {Puestos} from "../../../../../api/puestos/collection";
+import {Escuelas} from "../../../../../api/escuelas/collection";
 
 class AgregarTiendas {
     constructor($scope, $reactive) {
@@ -88,10 +88,6 @@ class AgregarTiendas {
             }
         }));
 
-    }
-
-    probando() {
-        console.log('PROBANDO, LO QUE SEA, IMPRÍMETE');
     }
 
     agregarTiendas(vacanteId) {
