19edb8a092b72d0cdba4b9ce6e48de7bbd3dff9a
jalevtz <j.vlt.mtz@gmail.com>
2017-02-15 19:53:57 -0600

    Componente que se utliza para agregar una tarjeta de conekta y de nuestra coleccion tarjetasBancarias

--- imports/ui/components/datosBancarios/agregarTarjeta/agregarTarjeta.js
+++ imports/ui/components/datosBancarios/agregarTarjeta/agregarTarjeta.js
@@ -7,31 +7,78 @@ import {Meteor} from "meteor/meteor";
 import {ValidationError} from "meteor/mdg:validation-error";
 import "./agregarTarjeta.html";
 import {name as Tarjeta} from "../tarjeta/tarjeta";
-import {insert} from "../../../../api/tarjetaBancaria/methods.js";
+import {name as Alertas} from "../../comun/alertas/alertas";
+import {agregarTarjeta} from "../../../../api/conekta/methods.js";
+import 'conekta.js/dist/conekta';
+
 
 class AgregarTarjeta {
-    constructor($scope, $reactive) {
+    constructor($scope, $reactive, $stateParams) {
         'ngInject';
-        this.titulo = 'Datos Bancarios';
-
         $reactive(this).attach($scope);
-        this.tarjeta = {};
-
-        this.respuesta = {
-            mostrar: false,
-            mensaje: '',
-            tipo: ''
-        };
+        Conekta.setPublicKey('key_INzsPzyiYTSRUfxQwdav5wg');
+        this.apiClienteId = $stateParams.clienteId;
+        console.log($stateParams.clienteId);
+        this.titulo = 'Datos Bancarios';
+        this.apiTokenId = '';
+        this.datos = {};
     }
+
+    generarToken() {
+        this.tipoMsj = '';
+        console.log('generarToken ' );
+        Conekta.Token.create(this.crearTarjetaConekta(), this.$bindToContext((token)=> {
+            console.log(token);
+            this.apiTokenId = token.id;
+            if ('4000000000000002' === this.datos.numeroTarjeta) {
+                this.apiTokenId = 'tok_test_card_declined'
+            } else if ('4000000000000127' === this.datos.numeroTarjeta) {
+                this.apiTokenId = 'tok_test_insufficient_funds'
+            } else if ('4012888888881881' === this.datos.numeroTarjeta) {
+                this.apiTokenId = 'tok_test_visa_1881'
+            } else if ('5105105105105100' === this.datos.numeroTarjeta) {
+                this.apiTokenId = 'tok_test_mastercard_5100'
+            } else if ('371449635398431' === this.datos.numeroTarjeta) {
+                this.apiTokenId = 'tok_test_amex_8431'
+            }
+            this.guardar();
+        }), this.$bindToContext((error)=> {
+            console.log(error);
+            this.tipoMsj = 'danger';
+            this.msj = error.message_to_purchaser;
+        }));
+    }
+
+    crearTarjetaConekta() {
+        const tarjetaAToken = {
+            address: {},
+            card: {
+                number: this.datos.numeroTarjeta,
+                name: this.datos.nombreTitular,
+                exp_year: this.datos.anioExpiracion,
+                exp_month: this.datos.mesExpiracion,
+                cvc: this.datos.codigoSeguridad
+            }
+        };
+        console.log(this.datos);
+        return tarjetaAToken;
+    }
+
     guardar() {
-        insert.call(this.tarjeta, this.$bindToContext((err) => {
-            this.respuesta.mostrar = true;
+        console.log(' this.apiTokenId ', this.apiTokenId);
+        console.log(' this.apiClienteId ', this.apiClienteId);
+        const tarjeta = {
+            apiTokenId: this.apiTokenId,
+            apiClienteId: this.apiClienteId
+        };
+
+        agregarTarjeta.call(tarjeta, this.$bindToContext((err) => {
             if (err) {
-                this.respuesta.mensaje = 'No se pudieron realizar los cambios bancarios.';
-                this.respuesta.tipo = 'danger';
+                this.msj = err.message;
+                this.tipoMsj = 'danger';
             } else {
-                this.respuesta.mensaje = 'Éxito al realizar los cambios bancarios.';
-                this.respuesta.tipo = 'success';
+                this.msj = 'Éxito al realizar los cambios bancarios.';
+                this.tipoMsj = 'success';
             }
         }));
     }
@@ -45,7 +92,8 @@ export default angular
         angularMeteor,
         uiRouter,
         ngAnimate,
-        Tarjeta
+        Tarjeta,
+        Alertas
     ])
     .component(name, {
         templateUrl: `imports/ui/components/datosBancarios/${name}/${name}.html`,
@@ -58,8 +106,8 @@ export default angular
 function config($stateProvider) {
     'ngInject';
     $stateProvider
-        .state('app.tarjeta', {
-            url: '/agregar',
+        .state('app.tarjetas.agregar', {
+            url: '/agregar/:clienteId',
             template: '<agregar-tarjeta></agregar-tarjeta>',
             resolve: {
                 currentUser($q) {
