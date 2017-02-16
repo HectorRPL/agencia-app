5f7fd06e5e92a71659366ebe344d40bc7c6b2fd7
jalevtz <j.vlt.mtz@gmail.com>
2017-02-15 18:11:03 -0600

    Modificación de los datos a presentar en la publicacion

--- imports/api/compras/bitacoraCompras/server/publish.js
+++ imports/api/compras/bitacoraCompras/server/publish.js
@@ -6,11 +6,12 @@ import {Meteor} from "meteor/meteor";
 import {BitacoraCompras} from '../collection';
 
 if (Meteor.isServer) {
-    Meteor.publish('bitacoraCompras.respuestaApi', function (compraId) {
-        return BitacoraCompras.find(compraId, {
+    Meteor.publish('bitacoraCompras.respuestaApi', function (id) {
+        return BitacoraCompras.find(id, {
             fields: {
                 apiRespuesta: 1,
-                apiDetalles: 1
+                apiDetalles: 1,
+                datosPeticion: 1
             }
         });
     });
