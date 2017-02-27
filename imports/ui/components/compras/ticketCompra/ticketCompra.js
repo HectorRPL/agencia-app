4f0c5b0006f7d8b593ed35920f5e74a1f26d4cea
jalevtz <j.vlt.mtz@gmail.com>
2017-02-27 13:14:00 -0600

    Se corrige la construcción de ticket,  se le envia toda la información de la compra para que solo despliegue.

--- imports/ui/components/compras/ticketCompra/ticketCompra.js
+++ imports/ui/components/compras/ticketCompra/ticketCompra.js
@@ -1,31 +1,26 @@
 /**
  * Created by jvltmtz on 16/11/16.
  */
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import './ticketCompra.html';
-import {BitacoraCompras} from '../../../../api/compras/bitacoraCompras/collection';
-import {DatosFinancieros} from '../../../../api/datosFinancieros/collection';
-import {InfoEmpresa} from '../../../../api/catalogos/infoEmpresa/collection';
+import angular from "angular";
+import angularMeteor from "angular-meteor";
+import "./ticketCompra.html";
+import {BitacoraCompras} from "../../../../api/compras/bitacoraCompras/collection";
+import {InfoEmpresa} from "../../../../api/catalogos/infoEmpresa/collection";
 
 class TicketCompra {
     constructor($scope, $reactive) {
         'ngInject';
         $reactive(this).attach($scope);
-        this.subscribe('datosFinancieros');
         this.subscribe('infoEmpresa');
         this.subscribe('bitacoraCompras.respuestaApi', ()=>[{_id: this.getReactively('id')}]);
         this.descripcion = '';
 
         this.helpers({
-            datosCompra(){
+            compra(){
                 return BitacoraCompras.findOne({_id: this.getReactively('id')});
             },
             infoEmpresa(){
                 return InfoEmpresa.findOne({_id: '1'});
-            },
-            datosFinancieros(){
-                return DatosFinancieros.findOne({_id: '1'});
             }
         });
     }
