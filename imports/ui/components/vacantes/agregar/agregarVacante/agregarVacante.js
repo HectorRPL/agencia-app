a16eae194cb6f33a97af1d20264b338acbb14ca2
hEctor <hector.kaizen@gmail.com>
2017-02-27 13:39:57 -0600

    [3 (6)] Issue Resuleto (demos): Las habilidades ya se están guardando. - El binding habseleccionadas="agregarVacante.listadoHab no acepta camel case, por eso se cambió a 'listado' - Se agrega la habilidad texto, ya que no se estaba guardando - Se corrige una etiqueta div que no estaba bien cerrada

--- imports/ui/components/vacantes/agregar/agregarVacante/agregarVacante.js
+++ imports/ui/components/vacantes/agregar/agregarVacante/agregarVacante.js
@@ -1,16 +1,16 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import uiRouter from 'angular-ui-router';
-import angularMessages from 'angular-messages';
-import './agregarVacante.html';
-import {name as CheckboxDias} from '../../../comun/checkBox/checkboxDias/checkboxDias';
-import {name as CheckboxHabilidades} from '../../../comun/checkBox/checkboxHabilidades/checkboxHabilidades';
-import {name as ElegirEstados} from '../../../comun/selects/elegirEstado/elegirEstado';
-import {name as agregarTienda} from '../agregarTiendas/agregarTiendas';
-import {name as  ElegirTalla} from '../../../comun/selects/elegirTalla/elegirTalla';
-import {name as  ElegirEscuela} from '../../../comun/selects/elegirEscuela/elegirEscuela';
-import {name as  ElegirPuesto} from '../../../comun/selects/elegirPuesto/elegirPuesto';
-import {Session} from 'meteor/session';
+import angular from "angular";
+import angularMeteor from "angular-meteor";
+import uiRouter from "angular-ui-router";
+import angularMessages from "angular-messages";
+import "./agregarVacante.html";
+import {name as CheckboxDias} from "../../../comun/checkBox/checkboxDias/checkboxDias";
+import {name as CheckboxHabilidades} from "../../../comun/checkBox/checkboxHabilidades/checkboxHabilidades";
+import {name as ElegirEstados} from "../../../comun/selects/elegirEstado/elegirEstado";
+import {name as agregarTienda} from "../agregarTiendas/agregarTiendas";
+import {name as  ElegirTalla} from "../../../comun/selects/elegirTalla/elegirTalla";
+import {name as  ElegirEscuela} from "../../../comun/selects/elegirEscuela/elegirEscuela";
+import {name as  ElegirPuesto} from "../../../comun/selects/elegirPuesto/elegirPuesto";
+import {Session} from "meteor/session";
 
 class AgregarVacante {
     constructor($scope, $reactive, $state) {
@@ -18,7 +18,7 @@ class AgregarVacante {
         $reactive(this).attach($scope);
         this.$state = $state;
 
-        if (Session.get('datosVacante') === null || Session.get('datosVacante') === undefined ) {
+        if (Session.get('datosVacante') === null || Session.get('datosVacante') === undefined) {
             this.vacante = {
                 perfil: {
                     habilidades: {
@@ -36,17 +36,15 @@ class AgregarVacante {
         } else {
             this.listadoHab = [];
             this.listadoDias = [];
-
-
             this.vacante = Session.get('datosVacante');
-
-        };
+        }
 
     }
 
     siguiente() {
         Session.setPersistent('datosVacante', this.vacante);
-        Session.setPersistent('habilidadesVacante', this.listadoHab);
+        Session.setPersistent('habilidadesVacanteText', this.vacante.perfil.habilidades.otra);
+        Session.setPersistent('habilidadesVacante', this.vacante.perfil.habilidades.listado);
         Session.setPersistent('diasVacante', this.listadoDias);
 
         this.$state.go('^.tiendas');
@@ -58,22 +56,22 @@ const name = 'agregarVacante';
 
 export default angular
     .module(name, [
-    angularMeteor,
-    uiRouter,
-    angularMessages,
-    CheckboxDias,
-    CheckboxHabilidades,
-    agregarTienda,
-    ElegirTalla,
-    ElegirEscuela,
-    ElegirPuesto,
-    ElegirEstados
-])
+        angularMeteor,
+        uiRouter,
+        angularMessages,
+        CheckboxDias,
+        CheckboxHabilidades,
+        agregarTienda,
+        ElegirTalla,
+        ElegirEscuela,
+        ElegirPuesto,
+        ElegirEstados
+    ])
     .component(name, {
-    templateUrl: `imports/ui/components/vacantes/agregar/${name}/${name}.html`,
-    controllerAs: name,
-    controller: AgregarVacante
-})
+        templateUrl: `imports/ui/components/vacantes/agregar/${name}/${name}.html`,
+        controllerAs: name,
+        controller: AgregarVacante
+    })
 
     .config(config);
 
